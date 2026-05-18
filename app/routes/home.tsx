83e4010a69ea5c8ca0dd886b6bc45aa1c005e1ae
yukimizu <yukidokemizu7189@gmail.com>
2026-05-18 15:01:55 +0900

    ogp2

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -59,7 +59,29 @@ export async function loader() {
 }
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "yukimizu | Portfolio" }];
+  const siteTitle = "Portfolio | yukimizu ";
+  const siteDescription = "yukimizuのポートフォリオサイトです。";
+  const siteUrl = "https://portfolio.yukidokemizu.com";
+  const imageUrl = `${siteUrl}/icon.jpg`;
+
+  return [
+    { title: siteTitle },
+    { name: "description", content: siteDescription },
+
+    { property: "og:title", content: siteTitle },
+    { property: "og:description", content: siteDescription },
+    { property: "og:type", content: "website" }, 
+    { property: "og:url", content: siteUrl },
+    { property: "og:image", content: imageUrl },
+
+    //X専用の設定
+    { name: "twitter:card", content: "summary" }, 
+    { name: "twitter:site", content: "@yukimizu7189" }, 
+    { name: "twitter:title", content: siteTitle },
+    { name: "twitter:description", content: siteDescription },
+    { name: "twitter:image", content: imageUrl },
+    { title: "yukimizu | Portfolio" }
+  ];
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
